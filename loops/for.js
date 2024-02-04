e8d10e284da160109f1aa42ab4d87a954c23306c
Carlos <148721970+dudu9bre@users.noreply.github.com>
2024-02-04 16:04:02 -0300

    grkkgrgk

---
+++ loops/for.js
@@ -0,0 +1,5 @@
+let tabuada = 7
+
+for(let c = 1; c <= 10; c++) {
+    console.log(tabuada, "x", c, "=", tabuada * c)
+}
---
+++ loops/while.js
@@ -0,0 +1,6 @@
+let tabuada = 7
+let c = 1
+
+while (c <= 10) {
+    console.log(tabuada, "x", c, "=", tabuada * c++)
+}
